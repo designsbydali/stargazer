7e0435ef4db7446dd74fab943494666d8d56f216
Shirish Dali <designsbydali@gmail.com>
2016-08-12 08:57:00 -0500

    final jvascript stargazer

--- js/stargazer.js
+++ js/stargazer.js
@@ -1,5 +1,36 @@
-function search(name) {
-  var api = 'http://www.strudel.org.uk/lookUP/json/?name='+name;
-  // requires "jsonp" datatype.
-  
+// requires "jsonp" datatype.
+
+$(document).ready(function() {
+  // Stuff to do as soon as the DOM is ready
+  $('#search').on('submit', function() {
+
+    var name = $('#search-name').val();
+    var api = 'http://www.strudel.org.uk/lookUP/json/?name=' + name;
+
+    console.log(name);
+
+
+    $.ajax({
+        url: api,
+        dataType: "jsonp",
+        jsonpCallback: "logResults"
+
+      })
+      // .done(function(info) {
+      //   console.log(info);
+      // })
+      .fail(function() {
+        alert("error");
+      });
+
+    return false;
+  })
+
+});
+
+function logResults(json) {
+  // console.log(json);
+
+  $('#preview').html('<img src="' + json.image.src + '">')
+
 }
